e5a3d2d3f5156e287c10b9d9cd857e6b202d739c
Rafael Cavazza <rafaelcavazza@gmail.com>
2016-08-25 21:55:28 -0300

    - Ajueste do namespace para poder contruir o projeto.

--- Presentation/Startup.cs
+++ Presentation/Startup.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-using Infra.Data.Context;
+using Infra.Context;
 
 namespace Presentation
 {
