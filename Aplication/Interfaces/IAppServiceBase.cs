279beaa4b769b8cbf78201acb1eafcd6ad898d90
Rafael Cavazza <rafaelcavazza@gmail.com>
2016-09-17 16:28:23 -0300

    Iniciando contrução da camda de Aplicão

---
+++ Aplication/Interfaces/IAppServiceBase.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+
+namespace Aplication.Interfaces
+{
+    public interface IAppServiceBase<TEntity> : IDisposable where TEntity : class
+    {
+        void Add(TEntity entity);
+        void Update(TEntity entity);
+        void Remove(TEntity entity);
+        TEntity GetById(Guid id);    
+        IEnumerable<TEntity> GetAll();   
+    }
+}
