09e3aabd06a3bec3b087b07c05e254e07e858c97
RafaelCavazza <rafaelcavazza@gmail.com>
2016-09-30 00:24:00 -0300

    - Adicionada nova migration.

--- Infra/Migrations/20160930032320_Migration6.Designer.cs
+++ Infra/Migrations/20160930032320_Migration6.Designer.cs
@@ -8,9 +8,10 @@ using Infra.Data.Context;
 namespace Infra.Migrations
 {
     [DbContext(typeof(DataBaseContext))]
-    partial class DataBaseContextModelSnapshot : ModelSnapshot
+    [Migration("20160930032320_Migration6")]
+    partial class Migration6
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
             modelBuilder
                 .HasAnnotation("ProductVersion", "1.0.0-rtm-21431")
---
+++ Infra/Migrations/20160930032320_Migration6.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Infra.Migrations
+{
+    public partial class Migration6 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "DateOfBirth",
+                table: "Employee",
+                nullable: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "DateOfBirth",
+                table: "Employee",
+                nullable: false);
+        }
+    }
+}
