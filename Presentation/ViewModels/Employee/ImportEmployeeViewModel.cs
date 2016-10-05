6f82586e6c7fe7c530f41e193b3379e23f58751e
Rafael Cavazza <rafaelcavazza@gmail.com>
2016-10-05 18:27:13 -0300

    Ajustes na View de Importação de Funcionários.

---
+++ Presentation/ViewModels/Employee/ImportEmployeeViewModel.cs
@@ -0,0 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Http;
+
+namespace Presentation.ViewModels.Employee
+{
+    public class ImportEmployeeViewModel
+    {
+        [DisplayAttribute(Name="Arquivo Para Importação")]
+        [DataType(DataType.Upload)]
+        [Required(ErrorMessage="O arquivo para Importação é obrigatório.")]
+        public IFormFile file {get; set;}
+    }
+}
