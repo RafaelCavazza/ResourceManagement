16bc2d9f7a54113813b98c31c173ae520d3f2f13
Rafael Cavazza <rafaelcavazza@gmail.com>
2016-10-24 20:31:56 -0200

    Refatoração método de geração de password.

--- Domain/Entities/User.cs
+++ Domain/Entities/User.cs
@@ -18,15 +18,10 @@ namespace Domain.Entities
             var random = new Random();
             var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
             var lowerCase = "abcdefghijklmnopqrstuvwxyz";
-            var number = "0123456789";
+            var numbers = "0123456789";
             var specialCaracters = "!@#$%*()_+{}[]?/:;><.,\\|'"; 
-            var length = 15;
-            var pass1 = new string(Enumerable.Repeat(upperCase, length).Select(s => s[random.Next(s.Length)]).ToArray());
-            var pass2 = new string(Enumerable.Repeat(lowerCase, length).Select(s => s[random.Next(s.Length)]).ToArray());
-            var pass3 = new string(Enumerable.Repeat(number, length).Select(s => s[random.Next(s.Length)]).ToArray());
-            var pass4 = new string(Enumerable.Repeat(specialCaracters, length).Select(s => s[random.Next(s.Length)]).ToArray());
-             
-            return pass1 + pass2 + pass3 + pass4; 
+            var passwordCharacters = upperCase + lowerCase + numbers + specialCaracters;
+            return new string(Enumerable.Repeat(passwordCharacters, 250).Select(s => s[random.Next(s.Length)]).ToArray());
         }
     }
 }
