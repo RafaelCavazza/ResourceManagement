d1e745912845d8ebe529ad3d76ed70d159e11f23
RafaelCavazza <rafaelcavazza@gmail.com>
2016-10-28 22:23:33 -0200

    Ajustes Remoção de Usings.

--- Domain/Interfaces/Repositories/IItemRepository.cs
+++ Domain/Interfaces/Repositories/IItemRepository.cs
@@ -1,4 +1,4 @@
-
+using Domain.Entities;
 
 namespace Domain.Interfaces.Repositories
 {
--- Infra/Data/Repositories/ItemRepository.cs
+++ Infra/Data/Repositories/ItemRepository.cs
@@ -1,8 +1,5 @@
-using System;
-using System.Linq;
 using Domain.Entities;
 using Domain.Interfaces.Repositories;
-using Microsoft.EntityFrameworkCore;
 
 namespace Infra.Data.Repositories
 {
