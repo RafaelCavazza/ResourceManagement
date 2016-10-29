d41af4286e967721aff932c88ebf4588afe0e234
Igor Santos Siqueira - TI <igor-ti@outlook.com>
2016-10-28 22:13:28 -0200

    Iniciação do Item

---
+++ Domain/Interfaces/Repositories/IItemRepository.cs
@@ -0,0 +1,8 @@
+
+
+namespace Domain.Interfaces.Repositories
+{
+    public interface IItemRepository : IRepositoryBase<Item>
+    {
+    }
+}
---
+++ Domain/Interfaces/Services/IItemService.cs
@@ -0,0 +1,9 @@
+using Domain.Entities;
+
+namespace Domain.Interfaces.Services
+{
+    public interface IItemService : IServiceBase<Item>
+    {
+        
+    }
+}
