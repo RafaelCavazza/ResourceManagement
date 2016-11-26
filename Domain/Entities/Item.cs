e8ca50a81e35c7fe6e00226360aa7a66c74e3428
Welbert Gomides <welbert.gomides@gmail.com>
2016-11-26 20:43:05 -0200

    Criação da entidade de Item

--- Domain/Entities/Item.cs
+++ Domain/Entities/Item.cs
@@ -4,10 +4,21 @@ namespace Domain.Entities
 {
     public class Item
     {
-        public Guid Id {get; set;}
-        public DateTime CreatedOn {get; set;}
-        public DateTime ModifiedOn {get; set;}
-        public Guid ProductId {get; set;}
-        public virtual Product Product {get; set;}
+        public Item()
+        {
+            this.Status = ItemStatus.Avaliable;
+        }
+
+        public Guid Id { get; set; }
+        public DateTime PurchasedOn { get; set; }
+        public string NF { get; set; }
+        public string SerialNumber { get; set; }
+        public string Patrimonio { get; set; }
+        public ItemStatus Status { get; set; }
+        public Guid ProductId { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public DateTime ModifiedOn { get; set; }
+        
+        public virtual Product Product { get; set; }
     }
 }
