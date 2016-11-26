13356dcc82a4504b9fd959267df4d44e1cb933f6
Welbert Gomides <welbert.gomides@gmail.com>
2016-11-26 21:18:05 -0200

    Configuração da entidade, criação da Migration

---
+++ Infra/Migrations/20161126230820_Migration15.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Infra.Migrations
+{
+    public partial class Migration15 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "NF",
+                table: "Item",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Patrimonio",
+                table: "Item",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "PurchasedOn",
+                table: "Item",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "SerialNumber",
+                table: "Item",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Status",
+                table: "Item",
+                nullable: false,
+                defaultValue: ItemStatus.0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "NF",
+                table: "Item");
+
+            migrationBuilder.DropColumn(
+                name: "Patrimonio",
+                table: "Item");
+
+            migrationBuilder.DropColumn(
+                name: "PurchasedOn",
+                table: "Item");
+
+            migrationBuilder.DropColumn(
+                name: "SerialNumber",
+                table: "Item");
+
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "Item");
+        }
+    }
+}
