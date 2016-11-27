e386ce0edccadf5889b53fa502d737c6243113c7
Welbert Gomides <welbert.gomides@gmail.com>
2016-11-27 21:44:38 -0200

    Removendo campos desnecessários do EditItemViewModel

--- Presentation/ViewModels/Item/EditItemViewModel.cs
+++ Presentation/ViewModels/Item/EditItemViewModel.cs
@@ -1,16 +1,11 @@
 using System;
 using System.ComponentModel.DataAnnotations;
-using Domain.Entities;
 
 namespace Presentation.ViewModels.Item
 {
     public class EditItemViewModel
     {
         public Guid Id { get; set; }
-        public DateTime PurchasedOn { get; set; }
-        public ItemStatus Status { get; set; }
-        public DateTime CreatedOn { get; set; }
-        public DateTime ModifiedOn { get; set; }
 
         [DisplayAttribute(Name = "Nota fiscal")]
         [DataType(DataType.Text)]
