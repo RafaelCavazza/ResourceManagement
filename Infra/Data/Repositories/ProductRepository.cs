2953b8063026514ec791c2a2c0b8cdb5a34a59d4
RafaelCavazza <rafaelcavazza@gmail.com>
2016-11-27 00:11:57 -0200

    Melhorias
    Adicionado ordenação em Products.
    Alterado nomeclatura do menu de Produtos.

--- Infra/Data/Repositories/ProductRepository.cs
+++ Infra/Data/Repositories/ProductRepository.cs
@@ -1,9 +1,16 @@
 using Domain.Entities;
 using Domain.Interfaces.Repositories;
+using Sakura.AspNetCore;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Infra.Data.Repositories
 {
     public class ProductRepository : RepositoryBase<Product>, IProductRepository
     {
+        public override IEnumerable<Product> GetPaged(int pageIndex, int pageSize)
+        {
+            return dbContext.Product.OrderBy(p=> p.CreatedOn).ToPagedList(pageSize, pageIndex);
+        }
     }
 }
