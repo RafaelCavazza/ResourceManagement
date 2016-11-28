3eda33c4c02607c5e9ddab8ffbfa0a76f11db861
RafaelCavazza <rafaelcavazza@gmail.com>
2016-11-27 23:23:04 -0200

    Loan Entity
    Adição de atributos a entidade Loan.
    Criação da classe de configuração do Repositório.

--- Domain/Entities/Loan.cs
+++ Domain/Entities/Loan.cs
@@ -5,6 +5,17 @@ namespace Domain.Entities
     public class Loan
     {
         public Guid Id { get; set; }
+
+        public Guid ItemId { get; set; }
+        public Guid EmployeeId { get; set; }
+
+        public virtual Item Item { get; set; }
+        public virtual Employee Employee { get; set; }
+        
+
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+
         public DateTime CreatedOn { get; set; }
         public DateTime ModifiedOn { get; set; }
     }
---
+++ Infra/Data/EntityConfig/LoanConfiguration.cs
@@ -0,0 +1,20 @@
+using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infra.Data.EntityConfig
+{
+    public class LoanConfiguration
+    {
+        public static void Configure(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Loan>(b =>
+                {
+                    b.HasKey(p=> p.Id);
+                    b.Property(p=> p.StartDate).IsRequired();
+                    b.Property(p=> p.EndDate).IsRequired();
+                    b.Property(p=> p.ItemId).IsRequired();
+                    b.Property(p=> p.Employee).IsRequired();
+                });            
+        }
+    }
+}
