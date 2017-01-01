a8130a7f7d8257e2507c599bd07e1cc1106d63f1
RafaelCavazza <rafaelcavazza@gmail.com>
2017-01-01 10:52:24 -0200

    Alteração planilha de importação. Criado ObjectValue para agripar dados da impotação.

--- Aplication/EmployeeAppService.cs
+++ Aplication/EmployeeAppService.cs
@@ -6,6 +6,7 @@ using Domain.Interfaces.Services;
 using Microsoft.AspNetCore.Http;
 using Aplication.Services.FileOperations;
 using System.Linq;
+using Aplication.ObjectValues;
 
 namespace Aplication
 {
@@ -43,26 +44,41 @@ namespace Aplication
         public IEnumerable<Tuple<string,bool>> ImportEmployees(IFormFile file)
         {
             var value = new List<Tuple<string,bool>>();
-
             var content = FileReader.ReadStringFormFile(file);
-            
-            //TODO: TEMINAR DE IMPLEMENTAR A IMPORTAÇÃO DE FUNCIONÁRIOS.
-            var lines = content.Split(new [] { Environment.NewLine }, StringSplitOptions.None);
-            var header = new[] { "FILIAL", "MATRICULA", "NOME", "CPF", "DATA", "ADMISSAO", "E-MAIL", "COORDENADOR" };
+            var employeesImport = MapEmployeeImport(content);
+            //Separar o arquivo em uma lista de Objetos com os valores
+            //Validar os Valores de cada elemento da lista
+            //Pupular essa lista com os valore de referência a outra entidade
+            //Enviar a lista para atualzação no banco
+            return value;
+        }
+
+        private List<EmployeeImport> MapEmployeeImport(string data)
+        {
+            var lines = data.Split(new [] { Environment.NewLine }, StringSplitOptions.None);
+            var header = new[] { "FILIAL", "MATRICULA", "NOME", "CPF", "DATA", "ADMISSAO", "E-MAIL"};
+            var emplyeesImport = new List<EmployeeImport>();
+
             foreach(var line in lines)
             {
                 if(header.Any(c => line.Contains(c)) )
                     continue;
 
                 var collumns = line.Split(';',',');
-                value.Add(new Tuple<string,bool> (collumns[2], true));
+                if(collumns.Length != 7)
+                {
+                    emplyeesImport.Add(new EmployeeImport
+                        {
+                            Status = false,
+                            StatusMessage = $"Número de colunas insuficiente para importação. Linha : { line } "
+                        }
+                    );
+                    continue;
+                }
+                
             }
-            
-            //Separar o arquivo em uma lista de Objetos com os valores
-            //Validar os Valores de cada elemento da lista
-            //Pupular essa lista com os valore de referência a outra entidade
-            //Enviar a lista para atualzação no banco
-            return value;
+
+            return emplyeesImport;
         }
     }
 }
