7268d3e3850df91d662713a0d90b1fca03d794bb
Rafael Cavazza Silva <rafaelcavazza@gmail.com>
2017-09-26 19:45:11 -0300

    Ajustes consulta item

--- Infra/Data/Repositories/ItemRepository.cs
+++ Infra/Data/Repositories/ItemRepository.cs
@@ -11,9 +11,13 @@ namespace Infra.Data.Repositories
     {
         public IEnumerable<Item> GetAllAvailableForLoan()
         {
-            return dbContext.Item
-            .Include(p=> p.Loans)
-            .Include(p=> p.Product).Where(i=> i.IsAvailableForLoan()).ToList();
+            return
+                from itemDb in dbContext.Item.Include(i=> i.Product)
+                where
+                    itemDb.Loans.All(l => l.HasFinished) &&
+                    itemDb.Status == ItemStatus.Avaliable
+                select
+                    itemDb;
         }
 
         public override IEnumerable<Item> GetPaged(int pageIndex, int pageSize)
