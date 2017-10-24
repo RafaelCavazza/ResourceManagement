d44fa688a9bd2d64b3cf66d62b681e78592794ed
Rafael Cavazza Silva <rafaelcavazza@gmail.com>
2017-10-24 20:37:35 -0200

    Added password test case.

---
+++ Test/Domain/UserTest.cs
@@ -0,0 +1,28 @@
+﻿using Domain.Entities;
+using System.Linq;
+using Xunit;
+
+namespace Test.Domain
+{
+    public class UserTest
+    {
+        [Fact]
+        public void PasswordTest()
+        {
+            var password = User.GenerateRandomPassword();
+
+            Assert.False(string.IsNullOrWhiteSpace(password));
+            Assert.True(ContaisLettersNumbersAndSpecialCaracters(password));
+        }
+
+        private bool ContaisLettersNumbersAndSpecialCaracters(string password)
+        {
+            var letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+            var numbers = "0123456789";
+            var specialCaracters = "!@#$%*()_+{}[]?/:;><.,\\|'";
+            var characters = letters + numbers + specialCaracters;
+
+            return characters.All(c => password.Contains(c));
+        }
+    }
+}
