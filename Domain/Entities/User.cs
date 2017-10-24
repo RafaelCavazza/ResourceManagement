248d9a5989e725d8ff6ade9a631a99b30a471044
Rafael Cavazza Silva <rafaelcavazza@gmail.com>
2017-10-24 20:37:04 -0200

    Improved Password Randomness

--- Domain/Entities/User.cs
+++ Domain/Entities/User.cs
@@ -17,9 +17,27 @@ namespace Domain.Entities
         {
             var random = new Random();
             var letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-            var numbersAndSpecialCaracters = "0123456789!@#$%*()_+{}[]?/:;><.,\\|'";
-            var passwordCharacters = letters + numbersAndSpecialCaracters;
-            return new string(Enumerable.Repeat(passwordCharacters, 250).Select(s => s[random.Next(s.Length)]).ToArray());
+            var numbers = "0123456789";
+            var specialCaracters = "!@#$%*()_+{}[]?/:;><.,\\|'";
+
+            return Shuffle(letters + numbers + specialCaracters);
+        }
+
+        private static string Shuffle(string str)
+        {
+            var array = str.ToCharArray();
+            var rng = new Random();
+            var n = array.Length;
+
+            while (n > 1)
+            {
+                n--;
+                var k = rng.Next(n + 1);
+                var value = array[k];
+                array[k] = array[n];
+                array[n] = value;
+            }
+            return new string(array);
         }
     }
 }
